d22976518e527357f44a25a163190c9bf950c76b
kazukk <kazuhiro.shimizu@gmail.com>
2015-05-20 16:15:11 +0900

    config change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
     :aws_secret_access_key  => ENV["aws_secret_access_key"],                        # required
-    :region                 => 'ap-northeast-1',                  # optional, defaults to 'us-east-1'
+    :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
     #:host                   => 's3.example.com',             # optional, defaults to nil
     #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     #:endpoint               => "https://s3.amazonaws.com"
