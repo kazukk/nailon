997690ba6825a8df44b94a0a17a492121e8cc570
kazukk <kazuhiro.shimizu@gmail.com>
2015-05-22 11:13:16 +0900

    names

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,4 +12,6 @@ mount_uploader :image, ImageUploader
 validates :name, :description, :price, presence: true
 validates :price, numericality: { greater_than: 0 }
 
+belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521003255) do
+ActiveRecord::Schema.define(version: 20150522014542) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150521003255) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
